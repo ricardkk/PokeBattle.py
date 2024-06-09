a2a0bd57201bf03a697357f89e0bd12dcc79b0d0
Ricardo <ricardomarinho638@gmail.com>
2024-06-09 00:28:54 -0300

    primeiro commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import tkinter as tk
+from tkinter import messagebox
+
+
+# miss = 100 - ataque.accuracy
+
+root = tk.Tk()
+root.title("PokeBattle.py")
+root.geometry("800x600")
+
+
+root.mainloop()
